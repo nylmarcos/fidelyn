7abfbd22f9a769ba50605df1c00e35005af08183
Nyl Marcos Soares Barbosa <nylmarcos@gmail.com>
2021-03-04 12:19:04 -0300

    adicionado token in ajax

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,13 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script type="text/javascript">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
 
     <title>{{ config('app.name', 'FideLyn') }}</title>
 
