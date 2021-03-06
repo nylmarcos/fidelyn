b4081d5dd8967297342723936afb7cfc6d4798f8
Nyl Marcos Soares Barbosa <nylmarcos@gmail.com>
2021-03-06 08:38:15 -0300

    bug bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,14 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script type="text/javascript">
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-    </script>
-
+    
     <title>{{ config('app.name', 'FideLyn') }}</title>
 
     <!-- Scripts -->
